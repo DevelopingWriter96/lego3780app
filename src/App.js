dc8f6d8575f719138c6b43f78da29d4e3f793ca2
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-04-16 17:30:23 -0600

    That Dang CORS Problem again...

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-// function getSets({setNumber}) {
-
-//     console.log(setNumber);
-    
-// }
-
 function App() {
 
-//   const [sets, setSets] = useState([]);
+  const [set, setSet] = useState([]);
  
-//   // useEffect(async () => {
+  useEffect(() => {
+  async function getSet() {
+    const resp = await fetch("https://brickset.com/api/v3.asmx/getSets", {
+      method: 'POST',
+      body: new URLSearchParams({
+          'apiKey': '3-qn95-6I0l-uhhJg',
+          'params': {'setNumber':'75325-1', 'extendedData':1},
+          'userHash': ''
+      })
+  })
+    const data = await resp.json();
 
-//   //   const resp = await fetch();
-//   //   const data = await resp.json();
+    console.log(data.results)
 
-//   // }, [])
+    setSet(data.results)
+  }
 
-  console.log('Is this working?');
+  getSet()
+
+  }, [])
 
   return (
           <p>I Love Lego!</p>
-
     )
   }
 
