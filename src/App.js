e1d8b5f069891e00d29747f1ad1ffb34b37aeda3
DevelopingWriter96 <staceyrichard1996@gmail.com>
2022-04-16 16:08:41 -0600

    Setting up api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I Love Lego!
-        </p>
-      </header>
-    </div>
-  );
+function getSets({setNumber}) {
+
+    console.log(setNumber);
+    
 }
+
+function App() {
+
+  const [sets, setSets] = useState([]);
+ 
+  useEffect(async () => {
+
+    const resp = await fetch();
+    const data = await resp.json();
+
+  }, [])
+
+  console.log('Is this working?');
+
+  return (
+          <p>I Love Lego!</p>
+
+    )
+  }
 
 export default App;
